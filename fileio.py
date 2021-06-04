b4cb9b1e8a3c664700f17f7e22d3586a1f6132de
David R <drodriguez8008@gmail.com>
2021-06-04 11:44:21 -0400

    Fixes issue #2

--- fileio.py
+++ fileio.py
@@ -3,11 +3,14 @@ import json
 import entity
 import pathlib as Path
 import jsonpickle
+import os
 
 player = entity.Player("bob", 5, 10, entity.Stats(2, 3, 4, 5))
 config = Config()
 
 def save(player, config):
+    if not os.path.isfile("saves/"):
+        os.mkdir("saves/")
     f = open("./saves/save1.json", "w")
     f.write(jsonpickle.encode([player, config], indent=4))
     f.close()
@@ -15,6 +18,10 @@ def save(player, config):
 
 def load(filename):
     #open and read the file after the appending:
+    if not os.path.isfile("saves/"):
+        os.mkdir("saves/")
+    if not os.path.isfile("saves/save1.json"): 
+        raise FileNotFoundError
     f = open(filename, "r")
     
     return jsonpickle.decode(f.read())
