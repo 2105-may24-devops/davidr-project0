ab86dafe2437608cfbfd695239ac13fae4114c4e
David R <drodriguez8008@gmail.com>
2021-06-04 16:25:47 -0400

    fixed making dir while loading and saving

--- fileio.py
+++ fileio.py
@@ -7,8 +7,8 @@ player = entity.Player("bob", 5, 10, entity.Stats(2, 3, 4, 5))
 config = Config()
 
 def save(player, config):
-    if not os.path.isfile("saves/"):
-        os.mkdir("saves/")
+    if not os.path.isdir("saves/"):
+        os.makedirs("saves/", exist_ok=True)
     f = open("./saves/save1.json", "w")
     f.write(jsonpickle.encode([player, config], indent=4))
     f.close()
@@ -16,8 +16,8 @@ def save(player, config):
 
 def load(filename):
     #open and read the file after the appending:
-    if not os.path.isfile("saves/"):
-        os.mkdir("saves/")
+    if not os.path.isdir("saves/"):
+        os.makedirs("saves/", exist_ok=True)
     if not os.path.isfile("saves/save1.json"): 
         raise FileNotFoundError
     f = open(filename, "r")
