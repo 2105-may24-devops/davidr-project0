f64d5236e0964c5c2034b819a3286512507db3d2
David R <drodriguez8008@gmail.com>
2021-06-07 18:11:51 -0400

    Added ansible files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import menus
+import sys
 from blessed import Terminal
 
 #   _____        __ _       _ _         _____                                     
@@ -12,8 +13,31 @@ from blessed import Terminal
 
 term = Terminal()
 
-print(term.home + term.clear + term.move_y(term.height // 2 - 10))
-lines = term.wrap("""    _____        __ _       _ _         _____                                     
+# non-interactive mode
+if len(sys.argv) > 1:
+  # print("Assuming direct control")
+  from entity import Player, Entity
+  import random
+
+  random.seed(1337)
+  player = Player()
+  enemy = Entity()
+
+  if sys.argv[1].lower() == "attack":
+    player.attack(enemy)
+    enemy.attack(player)
+  
+  if sys.argv[1].lower() == "guard":
+    player.guard()
+    enemy.attack(player)
+    enemy.guard()
+    player.attack(enemy)
+
+# interactive mode
+else:
+
+  print(term.home + term.clear + term.move_y(term.height // 2 - 10))
+  lines = term.wrap("""    _____        __ _       _ _         _____                                     
   |_   _|      / _(_)     (_) |       |  __ \                                   
     | |  _ __ | |_ _ _ __  _| |_ ___  | |  | |_   _ _ __   __ _  ___  ___  _ __ 
     | | | '_ \|  _| | '_ \| | __/ _ \ | |  | | | | | '_ \ / _` |/ _ \/ _ \| '_ \\
