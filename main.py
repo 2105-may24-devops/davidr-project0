bc549a974199d3f513225d0aeb06a3194b4e5c3f
David R <drodriguez8008@gmail.com>
2021-06-07 16:21:21 -0400

    Added control flow for interactive and non-interactive sections

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import menus
 from blessed import Terminal
 
@@ -12,25 +13,32 @@ from blessed import Terminal
 
 term = Terminal()
 
-print(term.home + term.clear + term.move_y(term.height // 2 - 10))
-lines = term.wrap("""    _____        __ _       _ _         _____                                     
+# non-interactive mode
+if len(sys.argv) > 1:
+  print("Assuming direct control")
+
+# interactive mode
+else:
+
+  print(term.home + term.clear + term.move_y(term.height // 2 - 10))
+  lines = term.wrap("""    _____        __ _       _ _         _____                                     
   |_   _|      / _(_)     (_) |       |  __ \                                   
     | |  _ __ | |_ _ _ __  _| |_ ___  | |  | |_   _ _ __   __ _  ___  ___  _ __ 
     | | | '_ \|  _| | '_ \| | __/ _ \ | |  | | | | | '_ \ / _` |/ _ \/ _ \| '_ \\
     _| |_| | | | | | | | | | | ||  __/ | |__| | |_| | | | | (_| |  __/ (_) | | | |
    |_____|_| |_|_| |_|_| |_|_|\__\___| |_____/ \__,_|_| |_|\__, |\___|\___/|_| |_|
                                                             __/ |                
-                                                           |___/                 """, width=82, drop_whitespace=False, replace_whitespace=False)
+                                                          |___/                 """, width=82, drop_whitespace=False, replace_whitespace=False)
 
-for line in lines:
-    print(term.green_on_black(term.center(line)))
+  for line in lines:
+      print(term.green_on_black(term.center(line)))
 
-print(term.home + term.move_y(term.height // 2))
-print(term.black_on_green(term.center('Press any key to continue.')))
+  print(term.home + term.move_y(term.height // 2))
+  print(term.black_on_green(term.center('Press any key to continue.')))
 
-with term.cbreak(), term.hidden_cursor():
-    term.inkey()
-    
-# Main Menu
+  with term.cbreak(), term.hidden_cursor():
+      term.inkey()
 
-menus.main_menu_loop()
+
+  # Main Menu
+  menus.main_menu_loop()
